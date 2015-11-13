1177df3679b1d9947dfc011fd85ab333fb5997d3
Maria Rodriguez <maria.rodriguez@synergy-gb.com>
2015-11-13 16:38:57 -0430

    Eliminacion de fixtures del .gitignore

---
+++ fixtures/data.js
@@ -0,0 +1,15 @@
+//Screen sample data (for development purposes)
+data = [
+    {
+        "title": "Casa",
+        "icon" : "http://i325.photobucket.com/albums/k362/gbsojo/Componente-05_zpsvffkhlxh.png"
+    },
+    {
+        "title": "Trabajo",
+        "icon" : "http://i325.photobucket.com/albums/k362/gbsojo/Componente-06_zpskpkv6dhk.png"
+    },
+    {
+        "title": "Universidad",
+        "icon" : "http://i325.photobucket.com/albums/k362/gbsojo/Componente-04_zpsrqu2v5om.png"
+    }
+];
